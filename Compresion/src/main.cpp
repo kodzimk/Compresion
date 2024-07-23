4ae1244a6a4c36e0ec56e06d47cba2f4f793db9a
kodzimk <proggamedev@gmail.com>
2024-07-23 13:04:05 +0500

    Add project files.

---
+++ Compresion/src/main.cpp
@@ -0,0 +1,157 @@
+#include <Windows.h>
+#include <string>
+#include <shobjidl.h> 
+#include<GL/glew.h>
+#include <GLFW/glfw3.h>
+#include<string>
+#include"..\libraries\imgui\imconfig.h"
+#include"..\libraries\imgui\imgui_impl_glfw.h"
+#include"..\libraries\imgui\imgui_impl_opengl3.h"
+#define GL_SILENCE_DEPRECATION
+
+
+std::string sSelectedFile;
+std::string sFilePath;
+bool openFile()
+{
+    //  CREATE FILE OBJECT INSTANCE
+    HRESULT f_SysHr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
+    if (FAILED(f_SysHr))
+        return FALSE;
+
+    // CREATE FileOpenDialog OBJECT
+    IFileOpenDialog* f_FileSystem;
+    f_SysHr = CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_ALL, IID_IFileOpenDialog, reinterpret_cast<void**>(&f_FileSystem));
+    if (FAILED(f_SysHr)) {
+        CoUninitialize();
+        return FALSE;
+    }
+
+    //  SHOW OPEN FILE DIALOG WINDOW
+    f_SysHr = f_FileSystem->Show(NULL);
+    if (FAILED(f_SysHr)) {
+        f_FileSystem->Release();
+        CoUninitialize();
+        return FALSE;
+    }
+
+    //  RETRIEVE FILE NAME FROM THE SELECTED ITEM
+    IShellItem* f_Files;
+    f_SysHr = f_FileSystem->GetResult(&f_Files);
+    if (FAILED(f_SysHr)) {
+        f_FileSystem->Release();
+        CoUninitialize();
+        return FALSE;
+    }
+
+    //  STORE AND CONVERT THE FILE NAME
+    PWSTR f_Path;
+    f_SysHr = f_Files->GetDisplayName(SIGDN_FILESYSPATH, &f_Path);
+    if (FAILED(f_SysHr)) {
+        f_Files->Release();
+        f_FileSystem->Release();
+        CoUninitialize();
+        return FALSE;
+    }
+
+    //  FORMAT AND STORE THE FILE PATH
+    std::wstring path(f_Path);
+    std::string c(path.begin(), path.end());
+    sFilePath = c;
+
+    //  FORMAT STRING FOR EXECUTABLE NAME
+    const size_t slash = sFilePath.find_last_of("/\\");
+    sSelectedFile = sFilePath.substr(slash + 1);
+
+    //  SUCCESS, CLEAN UP
+    CoTaskMemFree(f_Path);
+    f_Files->Release();
+    f_FileSystem->Release();
+    CoUninitialize();
+    return TRUE;
+}
+
+
+
+
+int main(void)
+{
+    if (!glfwInit())
+        return 1;
+
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+    // GL ES 2.0 + GLSL 100
+    const char* glsl_version = "#version 100";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(__APPLE__)
+    // GL 3.2 + GLSL 150
+    const char* glsl_version = "#version 150";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+#else
+    // GL 3.0 + GLSL 130
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+#endif
+
+    // Create window with graphics context
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Compresion Testing App", nullptr, nullptr);
+    if (window == nullptr)
+        return 1;
+
+    glfwMakeContextCurrent(window);
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsLight();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+#ifdef __EMSCRIPTEN__
+    ImGui_ImplGlfw_InstallEmscriptenCallbacks(window, "#canvas");
+#endif
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    while (!glfwWindowShouldClose(window))
+    {
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("Components", NULL, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_None);
+        ImGui::SetWindowPos(ImVec2(0, 0));
+
+        if (ImGui::Button("Load file"))
+            openFile();
+
+        ImGui::End();
+        ImGui::Render();
+        ImGui::EndFrame();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
